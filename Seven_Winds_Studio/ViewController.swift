4940b3fde665b437005b681da7831884217a32dc
Vladimir089 <pickerpikcer@gmail.com>
2024-11-02 13:25:24 +0300

    Initial Commit

---
+++ Seven_Winds_Studio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seven_Winds_Studio
+//
+//  Created by Владимир Кацап on 02.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
